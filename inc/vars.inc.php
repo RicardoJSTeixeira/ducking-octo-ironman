9b07664727dbfd92682fe5193de5531563706ed6
Vitor Correia <vhcorreia@gmail.com>
2015-05-26 17:43:23 +0100

    Submeter negativos #4, com a possibilidade de indicar a data de fidelização do cliente ao actual serviço contratado

--- inc/vars.inc.php
+++ inc/vars.inc.php
@@ -13,7 +13,7 @@
 define('APP_NAME', 'NOS Residencial');
 define('APP_COMPANY', 'Connecta Portugal');
 define('APP_VERSION', '1.2.0.4');
-define('APP_TYPE', 'DEV'); // DEV || PROD
+define('APP_TYPE', 'PROD'); // DEV || PROD
 define('APP_LASTUPDATE', '201505261741');
 define('APP_DATETIME_YMD', date('Y-m-d'));
 define('APP_DATETIME_NEXTWEEK', date('Y-m-d', time() + (7 * 24 * 60 * 60))); // http://php.net/manual/en/function.time.php
