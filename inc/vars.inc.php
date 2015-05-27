cfa01fc9cd41f7e60329bab4482323b81f09f2b3
vitor.correia <vhcorreia@gmail.com>
2015-05-27 17:39:33 +0100

    Adiçao nova matriz #3

--- inc/vars.inc.php
+++ inc/vars.inc.php
@@ -12,9 +12,9 @@
 
 define('APP_NAME', 'NOS Residencial');
 define('APP_COMPANY', 'Connecta Portugal');
-define('APP_VERSION', '1.2.0.6');
-define('APP_TYPE', 'DEV'); // DEV || PROD
-define('APP_LASTUPDATE', '201505271738');
+define('APP_VERSION', '1.2.0.7');
+define('APP_TYPE', 'PROD'); // DEV || PROD
+define('APP_LASTUPDATE', '201505271739');
 define('APP_DATETIME_YMD', date('Y-m-d'));
 define('APP_DATETIME_NEXTWEEK', date('Y-m-d', time() + (7 * 24 * 60 * 60))); // http://php.net/manual/en/function.time.php
 define('APP_DATETIME_TIMESTAMP', date('Y-m-d H:i:s'));
