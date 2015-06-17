86d4bdd6baf61f077710224057d19b64ee96ba9d
Vitor Correia <vhcorreia@gmail.com>
2015-06-17 18:26:35 +0100

    Adicionada a possibilidade de indicar quantos cartões estão agregados ao serviço actual

--- inc/vars.inc.php
+++ inc/vars.inc.php
@@ -12,9 +12,9 @@
 
 define('APP_NAME', 'NOS Residencial');
 define('APP_COMPANY', 'Connecta Portugal');
-define('APP_VERSION', '1.2.0.7');
-define('APP_TYPE', 'DEV'); // DEV || PROD
-define('APP_LASTUPDATE', '201506171818');
+define('APP_VERSION', '1.2.0.8');
+define('APP_TYPE', 'PROD'); // DEV || PROD
+define('APP_LASTUPDATE', '201506171826');
 define('APP_DATETIME_YMD', date('Y-m-d'));
 define('APP_DATETIME_NEXTWEEK', date('Y-m-d', time() + (7 * 24 * 60 * 60))); // http://php.net/manual/en/function.time.php
 define('APP_DATETIME_TIMESTAMP', date('Y-m-d H:i:s'));
