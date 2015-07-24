3e4c8354e46ed61750fcf9c24fe0eb514d0bfe25
Vitor Correia <vhcorreia@gmail.com>
2015-07-24 18:38:02 +0100

    Implementação da nova matriz de 2015-07-21

--- inc/vars.inc.php
+++ inc/vars.inc.php
@@ -12,7 +12,7 @@
 define('APP_NAME', 'NOS Residencial');
 define('APP_COMPANY', 'Connecta Portugal');
 define('APP_VERSION', '1.2.2.2');
-define('APP_TYPE', 'DEV'); // DEV || PROD
+define('APP_TYPE', 'PROD'); // DEV || PROD
 define('APP_LASTUPDATE', '201507241823');
 define('APP_DATETIME_YMD', date('Y-m-d'));
 define('APP_DATETIME_NEXTWEEK', date('Y-m-d', time() + (7 * 24 * 60 * 60))); // http://php.net/manual/en/function.time.php
